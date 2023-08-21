a598d55642b98b09ce8ab287c2819fee32baa075
mktcommunity <104900670+mktcommunity@users.noreply.github.com>
2023-08-21 20:25:04 +0100

    f

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,10 +1,14 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from 'next/image'
 export default function LoginButton() {
 	const { data: session } = useSession();
 	if (session) {
 		return (
 			<>
-				Signed in as {session.user.email} <br />
+				{/* Signed in as {session.user.name} <br />
+				UserID is as {session.user.id} <br /> */}
+				{/* User ID ´{session.user.id}´ <br /> */}
+				{/* <Image src="`${session.user.image}`" width={100} height={100}/><br /> */}
 				<button onClick={() => signOut()}>Sign out</button>
 			</>
 		);
@@ -12,7 +16,10 @@ export default function LoginButton() {
 	return (
 		<>
 			Not signed in <br />
-			<button onClick={() => signIn()}>Sign in</button>
+			{/* <button onClick={() => signIn()}>Sign in</button> */}
+			<button  onClick={() => signIn('twitter',{ callbackUrl: 'http://localhost:3000'})} className="cursor-pointer">
+      Sign Inu
+    </button>
 		</>
 	);
 }
