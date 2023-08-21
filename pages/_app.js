44faba2521235ebc987df11ac69a8b9006633abb
mktcommunity <marketing@communityfinance.io>
2023-08-21 15:33:34 +0100

    c

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import '../styles/globals.css';
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Header/>
-      <Component {...pageProps} />
-      <Footer />
-    </>
-  );
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import LoginButton from "../components/Login";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}) {
+	return (
+		<>
+			<SessionProvider session={session}>
+				<Header />
+				<LoginButton />
+				<Component {...pageProps} />
+				<Footer />
+			</SessionProvider>
+		</>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
-import Index from '../components/Index';
-import New from '../components/New';
+import React, { useState } from "react";
+import Index from "../components/Index";
+import New from "../components/New";
 
 export default function Home() {
-  const [activePage, setActivePage] = useState('index');
+	const [activePage, setActivePage] = useState("index");
 
-  const navigateToPage = (page) => {
-    setActivePage(page);
-  };
+	const navigateToPage = (page) => {
+		setActivePage(page);
+	};
 
-  return (
-    <>
-      {activePage === 'index' && <Index navigateToPage={navigateToPage} />}
-      {activePage === 'new' && <New navigateToPage={navigateToPage} />}
-    </>
-  );
+	return (
+		<>
+			{/* {activePage === 'index' && <Index navigateToPage={navigateToPage} />} */}
+			<div className="min-h-[50px] w-full flex justify-center items-center bg-red-600">
+				<span className="text-white">F*** you</span>
+			</div>
+
+			{/* {activePage === 'new' && <New navigateToPage={navigateToPage} />} */}
+		</>
+	);
 }
